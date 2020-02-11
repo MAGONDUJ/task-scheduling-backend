c13f63748e44813ccef4929bebcce92c354535d4
Julius Kariuki Magondu <kariukijulius63@gmail.com>
2020-02-11 13:34:02 +0300

    add heroku db url

--- config/database.js
+++ config/database.js
@@ -1,19 +1,29 @@
 const Sequelize = require("sequelize");
-console.log("DATABASE_URL", process.env.DATABASE_URL);
-module.exports = new Sequelize(
-  process.env.DB_DATABASE,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST || process.env.DATABASE_URL,
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+if (process.env.DATABASE_URL) {
+  module.exports = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
-    operatorsAliases: false,
-
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: true
     }
-  }
-);
+  });
+} else {
+  module.exports = new Sequelize(
+    process.env.DB_DATABASE,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: "postgres",
+      operatorsAliases: false,
+
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+      }
+    }
+  );
+}
