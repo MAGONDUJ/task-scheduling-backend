3388102291fa910aa57e67df50d0c42828d6725a
Julius Kariuki Magondu <kariukijulius63@gmail.com>
2020-02-11 13:38:09 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,4 @@
 const Sequelize = require("sequelize");
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
 if (process.env.DATABASE_URL) {
   module.exports = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
