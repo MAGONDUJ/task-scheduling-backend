a0db7f829dbd4d9cca5e7f0446649e9e95294c07
Julius Kariuki Magondu <kariukijulius63@gmail.com>
2020-02-11 12:13:27 +0300

    add heroku db url2

--- config/database.js
+++ config/database.js
@@ -5,9 +5,7 @@ module.exports = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host:
-      process.env.DB_HOST ||
-      "postgres://dkpvdmpgsghonf:a532fe5991ed58a6a49116d7da50faf6a05adfbea6ad443a93b0a033d1f1e86e@ec2-54-197-34-207.compute-1.amazonaws.com:5432/d3jpkv7qc32oic",
+    host: process.env.DB_HOST,
     dialect: "postgres",
     operatorsAliases: false,
 
